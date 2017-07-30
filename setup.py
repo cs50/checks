b04eeaa4804bdbe1851dce3e2566ec3df15a9b8e
Chad Sharp <cmlsharp@umich.edu>
2017-07-30 14:06:32 -0400

    Added install compatibility between python 2 and 3

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
+import os
 from setuptools import setup, find_packages
+import sys
 
-setup(
-    packages=find_packages(),
-    name="check50-checks",
-    include_package_data=True,
-    package_data = { '': ['*'] },
-)
+def main():
+    setup(
+        name="check50-checks",
+        include_package_data=True,
+        **setup50()
+    )
 
+
+def setup50():
+    if sys.version_info >= (3, 3):
+        with open("MANIFEST.in", "w") as f:
+            f.write("graft checks\n")
+        return {"packages": ["checks"]}
+
+    with open("checks/__init__.py", "w") as f:
+        f.write("__path__ = __import__(\"pkgutil\").extend_path(__path__, __name__)\n")
+
+    for root, dirs, files in os.walk("checks"):
+        if not "__init__.py" in files:
+            print(os.path.join(root, "__init__.py"))
+    return {"packages": find_packages(), "package_data": { "": "*" }}
+
+
+if __name__ == "__main__":
+    main()
