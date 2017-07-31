1833c9d312d5c5dfea870e624eb8150a78d7b8d5
Chad Sharp <cmlsharp@umich.edu>
2017-07-31 00:24:14 -0400

    Fix string concat bug

--- cs50/2017/x/recover/check50/__init__.py
+++ cs50/2017/x/recover/check50/__init__.py
@@ -3,7 +3,7 @@ from check50 import *
 
 class Recover(Checks):
     hashes = [
-        "c3ceda9dde5cf2212e96d6f61e2e849ac60e2c3cbdf0184f6e0c3b6ea5e69d31"
+        "c3ceda9dde5cf2212e96d6f61e2e849ac60e2c3cbdf0184f6e0c3b6ea5e69d31",
         "8c0bd07a6c232670fd55b50ac42611c349920a119ef575eb28f770b9c89753e4",
         "6a42aa4997a94d37d4087c26b705f876596999c426fedd0a927fd574b90cb760",
         "20b415907e05cc0e9b0ef2d9cbaac694578e01fa56967f578b2e6c9dc99f750d",
