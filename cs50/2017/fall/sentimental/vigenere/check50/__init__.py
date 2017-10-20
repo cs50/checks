5caff8262feb9c461900e10ec18c86474b5ccd14
David J. Malan <malan@harvard.edu>
2017-10-20 18:14:39 -0400

    s/compiles/exists/

--- cs50/2017/fall/sentimental/vigenere/check50/__init__.py
+++ cs50/2017/fall/sentimental/vigenere/check50/__init__.py
@@ -8,42 +8,42 @@ class Vigenere(Checks):
         """vigenere.py exists."""
         self.require("vigenere.py")
 
-    @check("compiles")
+    @check("exists")
     def aa(self):
         """encrypts "a" as "a" using "a" as keyword"""
         self.spawn("python vigenere.py a").stdin("a").stdout("ciphertext:\s*a\n", "ciphertext: a\n").exit(0)
 
-    @check("compiles")
+    @check("exists")
     def bazbarfoo_caqgon(self):
         """encrypts "barfoo" as "caqgon" using "baz" as keyword"""
         self.spawn("python vigenere.py baz").stdin("barfoo").stdout("ciphertext:\s*caqgon\n", "ciphertext: caqgon\n").exit(0)
 
-    @check("compiles")
+    @check("exists")
     def mixedBaZBARFOO(self):
         """encrypts "BaRFoo" as "CaQGon" using "BaZ" as keyword"""
         self.spawn("python vigenere.py BaZ").stdin("BaRFoo").stdout("ciphertext:\s*CaQGon\n", "ciphertext: CaQGon\n").exit(0)
 
-    @check("compiles")
+    @check("exists")
     def allcapsBAZBARFOO(self):
         """encrypts "BARFOO" as "CAQGON" using "BAZ" as keyword"""
         self.spawn("python vigenere.py BAZ").stdin("BARFOO").stdout("ciphertext:\s*CAQGON\n", "ciphertext: CAQGON\n").exit(0)
 
-    @check("compiles")
+    @check("exists")
     def bazworld(self):
         """encrypts "world!$?" as "xoqmd!$?" using "baz" as keyword"""
         self.spawn("python vigenere.py baz").stdin("world!$?").stdout("ciphertext:\s*xoqmd!\$\?\n", "ciphertext: xoqmd!$?\n").exit(0)
 
-    @check("compiles")
+    @check("exists")
     def noarg(self):
         """handles lack of argv[1]"""
         self.spawn("python vigenere.py").exit(1)
 
-    @check("compiles")
+    @check("exists")
     def toomanyargs(self):
         """handles argc > 2"""
         self.spawn("python vigenere.py 1 2 3").exit(1)
 
-    @check("compiles")
+    @check("exists")
     def reject(self):
         """rejects "Hax0r2" as keyword"""
         self.spawn("python vigenere.py Hax0r2").exit(1)
