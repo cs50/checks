8b49af1f23a4246b743f43ae75f689ec5a70768b
Erin Carvalho <emc@cs50.harvard.edu>
2017-10-25 17:01:08 -0400

    take 14

--- scramble/1/check50/__init__.py
+++ scramble/1/check50/__init__.py
@@ -25,6 +25,12 @@ class Scramble(Checks):
         self.spawn("./scramble 5").stdout("\s*E\s*A\s*Y\s*A\n\s*D\s*A\s*E\s*I\n\s*L\s*T\s*A\s*E\n\s*W\s*E\s*I\s*E", "  E A Y A\n  D A E I\n  L T A E\n  W E I E").stdout(">")
 
     @check("compiles")
+    def draw5(self):
+        """draws board 5 correctly"""
+        self.spawn("./scramble 5").stdout().stdin("line").stdout("score: 0")
+        @check("compiles")
+
+    @check("compiles")
     def lookup10(self):
         """finds and scores correctly"""
         self.add("log10.txt")
