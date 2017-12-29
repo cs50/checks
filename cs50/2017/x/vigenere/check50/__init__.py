01c3e71f59caa08c4337cf689af37d4d078335ef
Brenda Anderson <curiouskiwi@users.noreply.github.com>
2017-12-29 15:39:49 +1300

    Update __init__.py
    Added a check for the plaintext having a space (to test keyword increment)

--- cs50/2017/x/vigenere/check50/__init__.py
+++ cs50/2017/x/vigenere/check50/__init__.py
@@ -39,6 +39,11 @@ class Vigenere(Checks):
         self.spawn("./vigenere baz").stdin("world!$?").stdout("ciphertext:\s*xoqmd!\$\?\n", "ciphertext: xoqmd!$?\n").exit(0)
 
     @check("compiles")
+    def withspaces(self):
+        """encrypts "Hello, World!" as "Iekmo, Vprke!" using "baz" as keyword"""
+        self.spawn("./vigenere baz").stdin("Hello, World!").stdout("ciphertext:\s*Iekmo, Vprke!\n", "ciphertext: Iekmo, Vprke!\n").exit(0)
+    
+    @check("compiles")
     def noarg(self):
         """handles lack of argv[1]"""
         self.spawn("./vigenere").exit(1)
