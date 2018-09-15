90f859715844df9b058cd6584d7b65b87b16fbe7
Brian Yu <brianyu28@gmail.com>
2018-09-14 23:48:40 -0400

    pset1

---
+++ cs50/2018/fall/hello/check50/__init__.py
@@ -0,0 +1,25 @@
+import re
+
+from check50 import *
+
+class Hello(Checks):
+
+    @check()
+    def exists(self):
+        """hello.c exists."""
+        self.require("hello.c")
+
+    @check("exists")
+    def compiles(self):
+        """hello.c compiles."""
+        self.spawn("clang -std=c11 -o hello hello.c").exit(0)
+
+    @check("compiles")
+    def david(self):
+        """responds to name David."""
+        self.spawn("./hello").stdin("David").stdout("David", "David")
+
+    @check("compiles")
+    def brian(self):
+        """responds to name Brian."""
+        self.spawn("./hello").stdin("Brian").stdout("Brian", "Brian")
