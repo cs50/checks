6e586a2191322bddab875bb4a346694abc2159c4
Doug Lloyd <lloyd@cs50.harvard.edu>
2018-10-18 18:12:40 -0400

    sentimental checks

---
+++ cs50/2018/fall/sentimental/hello/check50/__init__.py
@@ -0,0 +1,21 @@
+import re
+
+from check50 import *
+
+class Hello(Checks):
+
+    @check()
+    def exists(self):
+        """hello.py exists."""
+        self.require("hello.py")
+
+    @check("exists")
+    def prints_hello(self):
+        """prints "hello, world\\n" """
+        expected = "[Hh]ello, world!?\n"
+        actual = self.spawn("python3 hello.py").stdout()
+        if not re.match(expected, actual):
+            err = Error(Mismatch("hello, world\n", actual))
+            if re.match(expected[:-1], actual):
+                err.helpers = "Did you forget a newline (\"\\n\") at the end of your string?"
+            raise err
