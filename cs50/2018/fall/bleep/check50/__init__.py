e3a60067372201d7cc3cd28ce8e685b13816bf3f
Doug Lloyd <lloyd@cs50.harvard.edu>
2018-10-19 16:13:59 -0400

    Update __init__.py

--- cs50/2018/fall/bleep/check50/__init__.py
+++ cs50/2018/fall/bleep/check50/__init__.py
@@ -14,34 +14,34 @@ class Bleep(Checks):
     @check("exists")
     def test_reject_no_args(self):
         """rejects len(sys.argv) less than 2"""
-        self.spawn("./bleep").exit(1)
+        self.spawn("python bleep.py").exit(1)
 
     @check("exists")
     def test_reject_many_args(self):
         """rejects len(sys.argv) more than 2"""
-        self.spawn("./bleep banned.txt banned.txt").exit(1)
+        self.spawn("python bleep.py banned.txt banned.txt").exit(1)
 
     @check("exists")
     def test_no_banned_words(self):
         """input of 'hello world' outputs 'hello world'"""
-        self.spawn("./bleep banned.txt").stdin("Hello world").stdout("Hello world\s*\n").exit(0)
+        self.spawn("python bleep.py banned.txt").stdin("Hello world").stdout("Hello world\s*\n").exit(0)
 
     @check("exists")
     def test_darn(self):
         """input of 'This darn world' outputs 'This **** world'"""
-        self.spawn("./bleep banned.txt").stdin("This darn world").stdout("This \*\*\*\* world\s*\n").exit(0)
+        self.spawn("python bleep.py banned.txt").stdin("This darn world").stdout("This \*\*\*\* world\s*\n").exit(0)
 
     @check("exists")
     def handles_capitalized(self):
         """input of 'THIS DARN WORLD' outputs 'THIS **** WORLD'"""
-        self.spawn("./bleep banned.txt").stdin("THIS DARN WORLD").stdout("THIS \*\*\*\* WORLD\s*\n").exit(0)
+        self.spawn("python bleep.py banned.txt").stdin("THIS DARN WORLD").stdout("THIS \*\*\*\* WORLD\s*\n").exit(0)
 
     @check("exists")
     def substrings(self):
         """doesn't censor substrings"""
-        self.spawn("./bleep banned.txt").stdin("Darning my socks").stdout("Darning my socks\s*\n").exit(0)
+        self.spawn("python bleep.py banned.txt").stdin("Darning my socks").stdout("Darning my socks\s*\n").exit(0)
 
     @check("exists")
     def handles_other_wordlists(self):
         """handles banned words lists with arbitrary words in them"""
-        self.spawn("./bleep banned2.txt").stdin("My cat and dog are great").stdout("My \*\*\* and \*\*\* are great\s*\n").exit(0)
+        self.spawn("python bleep.py banned2.txt").stdin("My cat and dog are great").stdout("My \*\*\* and \*\*\* are great\s*\n").exit(0)
