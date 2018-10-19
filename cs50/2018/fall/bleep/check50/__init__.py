99c69231a2027cfea423f4550287d527406add0b
Doug Lloyd <lloyd@cs50.harvard.edu>
2018-10-19 12:22:36 -0400

    bleep checks

--- cs50/2018/fall/bleep/check50/__init__.py
+++ cs50/2018/fall/bleep/check50/__init__.py
@@ -22,7 +22,7 @@ class Bleep(Checks):
 
     @check("exists")
     def test_no_banned_words(self):
-        """leaves phrases with no banned words alone"""
+        """input of 'hello world' outputs 'hello world'"""
         self.spawn("./bleep banned.txt").stdin("Hello world").stdout("Hello world\s*\n").exit(0)
 
     @check("exists")
@@ -31,6 +31,16 @@ class Bleep(Checks):
         self.spawn("./bleep banned.txt").stdin("This darn world").stdout("This \*\*\*\* world\s*\n").exit(0)
 
     @check("exists")
-    def handles_capitalizing(self):
+    def handles_capitalized(self):
         """input of 'THIS DARN WORLD' outputs 'THIS **** WORLD'"""
         self.spawn("./bleep banned.txt").stdin("THIS DARN WORLD").stdout("THIS \*\*\*\* WORLD\s*\n").exit(0)
+
+    @check("exists")
+    def substrings(self):
+        """doesn't censor substrings"""
+        self.spawn("./bleep banned.txt").stdin("Darning my socks").stdout("Darning my socks").exit(0)
+
+    @check("exists")
+    def handles_other_wordlists(self):
+        """handles banned words list with arbitrary words in them"""
+        self.spawn("./bleep banned2.txt").stdin("I own a cat and a dog").stdout("I own a \*\*\* and a \*\*\*\s*\n").exit(0)
