66f5ef287f9d1906458263ea94e8a454b85d0765
Brian Yu <brianyu28@gmail.com>
2018-10-20 23:55:10 -0400

    Add string version of stdout output

--- cs50/2018/fall/bleep/check50/__init__.py
+++ cs50/2018/fall/bleep/check50/__init__.py
@@ -24,24 +24,24 @@ class Bleep(Checks):
     @check("exists")
     def test_no_banned_words(self):
         """input of 'hello world' outputs 'hello world'"""
-        self.spawn("python bleep.py banned.txt").stdin("Hello world").stdout("Hello world\s*\n").exit(0)
+        self.spawn("python bleep.py banned.txt").stdin("Hello world").stdout("Hello world\s*\n", "Hello world\n").exit(0)
 
     @check("exists")
     def test_darn(self):
         """input of 'This darn world' outputs 'This **** world'"""
-        self.spawn("python bleep.py banned.txt").stdin("This darn world").stdout("This \*\*\*\* world\s*\n").exit(0)
+        self.spawn("python bleep.py banned.txt").stdin("This darn world").stdout("This \*\*\*\* world\s*\n", "This **** world\n").exit(0)
 
     @check("exists")
     def handles_capitalized(self):
         """input of 'THIS DARN WORLD' outputs 'THIS **** WORLD'"""
-        self.spawn("python bleep.py banned.txt").stdin("THIS DARN WORLD").stdout("THIS \*\*\*\* WORLD\s*\n").exit(0)
+        self.spawn("python bleep.py banned.txt").stdin("THIS DARN WORLD").stdout("THIS \*\*\*\* WORLD\s*\n", "THIS **** WORLD\n").exit(0)
 
     @check("exists")
     def substrings(self):
         """doesn't censor substrings"""
-        self.spawn("python bleep.py banned.txt").stdin("Darning my socks").stdout("Darning my socks\s*\n").exit(0)
+        self.spawn("python bleep.py banned.txt").stdin("Darning my socks").stdout("Darning my socks\s*\n", "Darning my socks\n").exit(0)
 
     @check("exists")
     def handles_other_wordlists(self):
         """handles banned words lists with arbitrary words in them"""
-        self.spawn("python bleep.py banned2.txt").stdin("My cat and dog are great").stdout("My \*\*\* and \*\*\* are great\s*\n").exit(0)
+        self.spawn("python bleep.py banned2.txt").stdin("My cat and dog are great").stdout("My \*\*\* and \*\*\* are great\s*\n", "My *** and *** are great\n").exit(0)
