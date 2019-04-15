33e18fb4449698616db2defb6688da218d7990be
Brian Yu <brianyu28@gmail.com>
2019-04-15 18:44:38 -0400

    Change name to mario

--- cs50/labs/pdss/3/loops/check50/__init__.py
+++ cs50/labs/pdss/3/loops/check50/__init__.py
@@ -4,20 +4,20 @@ class Loops(Checks):
 
     @check()
     def exists(self):
-        """loops.py submitted"""
-        self.require("loops.py")
+        """mario.py submitted"""
+        self.require("mario.py")
 
     @check("exists")
     def one_question(self):
         """prints one question mark when input is 1"""
-        self.spawn("python types.py").stdin("1").stdout("^\?\n", "?")
+        self.spawn("python mario.py").stdin("1").stdout("^\?\n", "?")
 
     @check("exists")
     def four_questions(self):
         """prints four question mark when input is 4"""
-        self.spawn("python types.py").stdin("4").stdout("^\?\?\?\?\n", "????")
+        self.spawn("python mario.py").stdin("4").stdout("^\?\?\?\?\n", "????")
 
     @check("exists")
     def eight_questions(self):
         """prints eight question mark when input is 8"""
-        self.spawn("python types.py").stdin("8").stdout("^\?\?\?\?\?\?\?\?\n", "????????")
+        self.spawn("python mario.py").stdin("8").stdout("^\?\?\?\?\?\?\?\?\n", "????????")
