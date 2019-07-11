0ec5327ceb8cea83581b7f2edb0d9aeb77bf6136
Kareem Zidane <kzidane@cs50.harvard.edu>
2019-07-11 13:15:52 -0400

    Update __init__.py

--- cs50/2019/x/sentimental/hello/check50/__init__.py
+++ cs50/2019/x/sentimental/hello/check50/__init__.py
@@ -12,9 +12,9 @@ class Hello(Checks):
     @check("exists")
     def david(self):
         """responds to name Veronica."""
-        self.spawn("python hello.py").stdin("Veronica").stdout("hello, Veronica\n")
+        self.spawn("python3 hello.py").stdin("Veronica").stdout("hello, Veronica\n")
 
     @check("exists")
     def brian(self):
         """responds to name Brian."""
-        self.spawn("python hello.py").stdin("Brian").stdout("hello, Brian\n")
+        self.spawn("python3 hello.py").stdin("Brian").stdout("hello, Brian\n")
